2aea3fdc787361bdf484c052eca70eebfe15a286
15939460109 <15939460109@163.com>
2020-09-15 16:38:57 +0800

    lombok插件使用2

--- springboot02configuration/src/main/java/com/czg/domain/Vegetables.java
+++ springboot02configuration/src/main/java/com/czg/domain/Vegetables.java
@@ -8,28 +8,4 @@ public class Vegetables {
     private String potato;
     private String eggplant;
     private String greenPepper;
-
-    public String getPotato() {
-        return potato;
-    }
-
-    public void setPotato(String potato) {
-        this.potato = potato;
-    }
-
-    public String getEggplant() {
-        return eggplant;
-    }
-
-    public void setEggplant(String eggplant) {
-        this.eggplant = eggplant;
-    }
-
-    public String getGreenPepper() {
-        return greenPepper;
-    }
-
-    public void setGreenPepper(String greenPepper) {
-        this.greenPepper = greenPepper;
-    }
 }
