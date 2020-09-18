b30202909d343e273e49a8a8e4902b9a3cc33783
15939460109 <15939460109@163.com>
2020-09-18 14:56:23 +0800

    SpringBootWeb开发 Thymeleaf初始

---
+++ springboot-03-template/src/main/java/com/czg/DemoApplication3.java
@@ -0,0 +1,13 @@
+package com.czg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication3 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication3.class, args);
+    }
+
+}
---
+++ springboot-03-template/src/main/java/com/czg/domain/Restaurant.java
@@ -0,0 +1,11 @@
+package com.czg.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class Restaurant {
+
+    private String boss;
+    private String chef;
+}
