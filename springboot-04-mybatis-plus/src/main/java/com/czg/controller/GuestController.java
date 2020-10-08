e1edc0e03fa31e9746f029337dac0c7e9e39bac1
15939460109 <15939460109@163.com>
2020-10-08 22:16:45 +0800

    springboot事务之@Transactional注解

--- springboot-04-mybatis-plus/src/main/java/com/czg/controller/GuestController.java
+++ springboot-04-mybatis-plus/src/main/java/com/czg/controller/GuestController.java
@@ -4,8 +4,10 @@ import com.czg.model.Guest;
 import com.czg.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +24,19 @@ public class GuestController {
         List<Guest> guestList = guestService.list();
         model.addAttribute("guestList", guestList);
         return "list";
+    }
+
+    @GetMapping("/toAdd")
+    public String toAdd() {
+        return "add";
+    }
+
+    //开启事务
+    @Transactional
+    @PostMapping("/add")
+    public String add(Guest guest) {
+        guestService.save(guest);
+        //int a = 1 / 0;
+        return "redirect:/guest";
     }
 }
