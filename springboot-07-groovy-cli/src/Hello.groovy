a6f8cd5cdb79da5d44c5e58e553f1310988bf558
15939460109 <15939460109@163.com>
2020-12-14 17:58:51 +0800

    开发实战-Groovy初识&CLI

---
+++ springboot-07-groovy-cli/src/Hello.groovy
@@ -0,0 +1,6 @@
+
+// groovy通过变量内容猜测变量类型
+def value = "hello world"
+// value = 123
+println(value)
+println(value.class)
---
+++ springboot-07-groovy-cli/src/HelloCLI.groovy
@@ -0,0 +1,11 @@
+//@Grab("org.springframework.boot:spring-boot-starter-thymeleaf:2.2.12.RELEASE")
+//@Grab("org.webjars.bower:jquery:3.5.1")
+//@Controller
+//class HelloCLI {
+//
+//    @RequestMapping("/")
+//    String hello(Model model) {
+//        model.addAttribute("name", "Hello World2!")
+//        return "welcome"
+//    }
+//}
